761c316fefe726b7eb6d597a3374f522f94d76e4
Luana Silva <luana.dossantos@zup.com.br>
2024-12-04 14:45:26 -0300

    feat: adiciona laço de repetição While

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        // 2 -Faça um laço de repetição utilizando for que imprime na tela os números de um vetor com 5 elementos
+        // 2 - Faça um laço de repetição utilizando for que imprime na tela os números de um vetor com 5 elementos
         int[] numeros = {4, 8, 15, 16, 23};
 
         for (int i = 0; i < numeros.length; i++) {
             System.out.println(numeros[i]);
         }
+
+        System.out.println(" ");
+        // 3 - Faça um laço de repetição utilizando while que imprime na tela os números de um vetor com 5 elementos
+        int i = 0;
+        while (i < numeros.length) {
+            System.out.println(numeros[i] + " ");
+            i++;
+        }
+
     }
 }
