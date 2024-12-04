d70695bfb924fc716cce89e1cc03cc612cdce138
Luana Silva <luana.dossantos@zup.com.br>
2024-12-04 14:16:38 -0300

    feat: adiciona laço de repeticao para vetor

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // 2 -Faça um laço de repetição utilizando for que imprime na tela os números de um vetor com 5 elementos
+        int[] numeros = {4, 8, 15, 16, 23};
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+    }
+}
